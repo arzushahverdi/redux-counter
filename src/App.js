29a5aafd161294ae06670cfa934fd2dece259f11
Arzu Shahverdiyeva <arzu.shahverdi@gmail.com>
2024-06-27 17:12:42 +0400

    Pushing project to the Github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./counterSlice";
 
 function App() {
+  const { value } = useSelector((store) => store.counter);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "50px" }}>
+      <h1>Value: {value}</h1>
+      <button
+        onClick={() => dispatch(increment())}
+        style={{ marginRight: "20px" }}
+      >
+        Increase
+      </button>
+      <button onClick={() => dispatch(decrement())}>Decrease</button>
     </div>
   );
 }
